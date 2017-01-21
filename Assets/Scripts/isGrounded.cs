0396066611ac5db548bf70fdd5fb2cf237a85c7d
Lerkus <Sebastian-Maag@gmx.de>
2017-01-21 01:45:06 +0100

    now i want to sleep
    Long live the Spoonsnakes!

---
+++ Assets/Scripts/isGrounded.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isGrounded : MonoBehaviour
+{
+
+    private bool grounded = true;
+    public bool _grounded
+    {
+        get { return grounded; }
+        private set { }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        grounded = true;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        grounded = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        grounded = false;
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool jumping = false;
+    public float jumpHightTweaker = 10;
+    public float wallkingTweaker = 1;
+    public float flyTweaker = 0.5f;
+
+    public isGrounded grounder;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetAxisRaw("Jump") == 1 && grounder._grounded)
+        {
+            jump();
+        }
+
+        horizontalMove();
+    }
+
+    private void horizontalMove()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+
+        if (!grounder._grounded)
+        {
+            input *= flyTweaker;
+        }
+        else
+        {
+            input *= wallkingTweaker;
+        }
+
+        rb.AddForce(input, ForceMode.VelocityChange);
+    }
+
+    public void jump()
+    {
+        rb.AddForce(new Vector3(0, jumpHightTweaker, 0), ForceMode.VelocityChange);
+    }
+}

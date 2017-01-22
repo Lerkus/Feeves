4c59c9faccb55ae5451c5c14b58553ebb804127b
Korbinian Maag <korbinian.maag@gmail.com>
2017-01-22 13:48:13 +0100

    MainMenu mehr premium gemacht
    krass premium

---
+++ Assets/Main Menu/MoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    public bool MoveLeft = true;
+    public float TransformPositon = 30;
+    public int speed = 2;
+
+    public void MOVEIT()
+    {
+
+
+    }
+
+
+}
